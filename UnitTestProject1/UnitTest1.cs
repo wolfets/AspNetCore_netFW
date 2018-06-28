03ea1f571cda7a50d48ad89364f93c9bb34c58e9
STD <wolfets@free.fr>
2018-06-28 11:32:57 +0200

    ...

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -2,6 +2,8 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MvvmLight1.Model;
 using System.Diagnostics;
+using System.IO;
+using System.Xml.Linq;
 
 //https://msdn.microsoft.com/fr-fr/library/ms182532.aspx#BKMK_Create_a_unit_test_project
 //https://docs.microsoft.com/fr-fr/dotnet/core/testing/unit-testing-with-mstest
@@ -58,8 +60,23 @@ namespace UnitTestProject1
             //Assert.IsFalse(ctrl != null, "1 should not be prime");
         }
 
+        [TestMethod]
+        public void loadXML()
+        {
+            //Arrange
+            //string filePath = @"C:\Users\STD\Documents\GitHub\AspNetCore_netFW\UnitTestProject1\UnitTest1.xml";
+            string filePath = Path.Combine("", @"UnitTest1.xml.xml");
 
+            //Act
+            XDocument document = XDocument.Load(filePath);
 
+            //Assert
+            Assert.IsTrue(document.Elements() != null);
+            Debug.WriteLine(DateTime.Now.TimeOfDay.ToString());
+        }
+
+        //https://mikaelkrief.wordpress.com/2011/11/25/c-les-tests-unitaires-part-4-et-fin-les-tests-unitaire-et-la-base-de-donnees/
+        // tests base de données à faire
 
     }
 }
